1697c2605b75566ffd6b69a86da086213e77046a
vvchetin <35876049+vvchetin@users.noreply.github.com>
2018-01-29 05:01:35 +0500

    Добавил ссылку на обратную связь

--- rabotaWebandBD/www/tr.php
+++ rabotaWebandBD/www/tr.php
@@ -134,6 +134,11 @@ $result1=mysql_query("SELECT `coach`.id,`coach`.Name,`coach`.Gender, `Sport`.spn
 <br>
 </td>
 </tr>
+	<tr>
+		<td colspan="4">
+			<a href="obr.html" title="Связаться с нами!"><p class=tex>ОБРАТНАЯ СВЯЗЬ</p> </a>
+		</td>
+	</tr>
 </table>
 <table width=100% height=10% cellspacing="0">
 <tr width=1% height=1% bgcolor=#d1d1d1  >
